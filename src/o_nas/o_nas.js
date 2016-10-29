0936568dbf7e59f36ffa916fcb2405f52978800d
marek <mzydlewski@wp.pl>
2016-10-29 23:24:52 +0200

    fragment pt. "o nas"

---
+++ src/o_nas/o_nas.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Marek on 28.10.2016.
+ */
+var React = require('react');
+//var ReactDOM = require('react-dom');
+
+var Piotrek = {
+    title: "...has not delivered the description of himself",
+    src: img/piotrek.gif
+};
+
+var Grzegorz = {
+    title: "Computer graphics, desktop publishing and websites design are what he feels most comfortable with",
+    src: img/grzegorz1.png
+};
+
+var Marek = {
+    title: "The one who loves to spend his time in an active way, extremely keen on new technologies.",
+    src: img/marek1.png
+};
+
+var persons = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <p>{Piotrek.title}</p>
+                <img src = {Piotrek.src}/>
+
+                <p>{Grzegorz.title}</p>
+                <img src = {Grzegorz.src}/>
+            
+                <p>{Marek.title}</p>
+            <img src = {Marek.src}/>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <persons />,
+    document.getElementsByClassName('app')
+);
