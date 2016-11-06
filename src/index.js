a9a06522bb64324e8ff3678576fec22ff1f91b58
piotrandrzejewski <andrzejewskipiotr.me@gmail.com>
2016-11-06 17:21:15 +0100

    -creating store -update : (bootstrap,react-redux)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import {
+    Router,
+    Route,
+    browserHistory
+} from 'react-router';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import { Provider } from 'react-redux';
+//import store from './store'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Provider >
+        <Router history={browserHistory}>
+            <Route path="/" component={App} />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
