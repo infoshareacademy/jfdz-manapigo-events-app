217f4d0634e02b3d643d7b6b37da14cd0934bd91
marek <mzydlewski@wp.pl>
2016-11-20 13:55:40 +0100

    o_nas: zmniejszenie rozdzielczości zdjęć (katalog img)

---
+++ src/authors/o_nas.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Marek on 28.10.2016.
+ */
+var React = require('react');
+//var ReactDOM = require('react-dom');
+
+import piotrekSrc from './img/piotrek.gif'
+import grzegorzSrc from './img/grzegorz1.png'
+import marekSrc from './img/marek1.png'
+
+var Piotrek = {
+    title: "...has not delivered the description of himself",
+    src: piotrekSrc
+};
+
+var Grzegorz = {
+    title: "Computer graphics, desktop publishing and websites design are what he feels most comfortable with",
+    src: grzegorzSrc
+};
+
+var Marek = {
+    title: "The one who loves to spend his time in an active way, extremely keen on new technologies.",
+    src: marekSrc
+};
+
+var Persons = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <p>{Piotrek.title}</p>
+                <img src = {Piotrek.src}/>
+
+                <p>{Grzegorz.title}</p>
+                <img src = {Grzegorz.src}/>
+            
+                <p>{Marek.title}</p>
+            <img src = {Marek.src}/>
+            </div>
+        );
+    }
+});
+
+export default Persons;
--- src/o_nas/o_nas.js
+++ src/o_nas/o_nas.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid, Row, Col, Image, } from 'react-bootstrap';
+import {Grid, Row, Col, Image,} from 'react-bootstrap';
 import PiotrekUrl from './img/herb.png';
 import GrzegorzUrl from './img/grzegorz.jpg';
 import MarekUrl from './img/marek.jpg';
@@ -10,15 +10,16 @@ const authorsPhotos = (props) => (
         <Row>
             <Col xs={12} md={4} lg={3}>
                 <Image src={PiotrekUrl} rounded/>
-                
+
             </Col>
             <Col xs={12} md={4} lg={3}>
-                <Image src={GrzegorzUrl} rounded />
-                 
+                <Image src={GrzegorzUrl} rounded/>
+
             </Col>
             <Col xs={12} md={4} lg={3}>
-                <Image src={MarekUrl} rounded />
-                
+                <div style={{textAlign: 'center'}}>
+                    <Image src={MarekUrl} rounded/>
+                </div>
             </Col>
         </Row>
     </Grid>
@@ -29,47 +30,46 @@ export default authorsPhotos;
 //ReactDOM.render(photoAuthors, mountNode);
 
 
-
 /*
-var React = require('react');
-//var ReactDOM = require('react-dom');
+ var React = require('react');
+ //var ReactDOM = require('react-dom');
 
-import piotrekSrc from './img/piotrek.gif'
-import grzegorzSrc from './img/grzegorz1.png'
-import marekSrc from './img/marek1.png'
+ import piotrekSrc from './img/piotrek.gif'
+ import grzegorzSrc from './img/grzegorz1.png'
+ import marekSrc from './img/marek1.png'
 
-var Piotrek = {
-    title: "...has not delivered the description of himself",
-    src: piotrekSrc
-};
+ var Piotrek = {
+ title: "...has not delivered the description of himself",
+ src: piotrekSrc
+ };
 
-var Grzegorz = {
-    title: "Computer graphics, desktop publishing and websites design are what he feels most comfortable with",
-    src: grzegorzSrc
-};
+ var Grzegorz = {
+ title: "Computer graphics, desktop publishing and websites design are what he feels most comfortable with",
+ src: grzegorzSrc
+ };
 
-var Marek = {
-    title: "The one who loves to spend his time in an active way, extremely keen on new technologies.",
-    src: marekSrc
-};
+ var Marek = {
+ title: "The one who loves to spend his time in an active way, extremely keen on new technologies.",
+ src: marekSrc
+ };
 
-var Persons = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <p>{Piotrek.title}</p>
-                <img src = {Piotrek.src}/>
+ var Persons = React.createClass({
+ render: function() {
+ return (
+ <div>
+ <p>{Piotrek.title}</p>
+ <img src = {Piotrek.src}/>
 
-                <p>{Grzegorz.title}</p>
-                <img src = {Grzegorz.src}/>
+ <p>{Grzegorz.title}</p>
+ <img src = {Grzegorz.src}/>
 
-                <p>{Marek.title}</p>
-            <img src = {Marek.src}/>
-            </div>
-        );
-    }
-});
+ <p>{Marek.title}</p>
+ <img src = {Marek.src}/>
+ </div>
+ );
+ }
+ });
 
-export default Persons;
+ export default Persons;
 
-*/
+ */
