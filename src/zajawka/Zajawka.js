e9fab1524b3f2e2738e200b9bbdff531b77f014c
grzegorzbelter <greniu84@wp.pl>
2016-11-26 13:19:35 +0100

    Dodanie zdjec zajawki, pliku .css i ostylowanie zajawki

--- src/zajawka/Zajawka.js
+++ src/zajawka/Zajawka.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
 import { Carousel } from 'react-bootstrap';
+import './zajawka.css';
 import dluga from './img/dluga.jpg';
+import glowny from './img/glowny.jpg';
+import karuzela from './img/karuzela.jpg';
 
 const zdjecia = (props) => (
     <Carousel>
         <Carousel.Item>
-            <img width={1200} height={500} alt="Gdansk" src={dluga}/>
+            <img alt="Gdansk" src={glowny}/>
         </Carousel.Item>
         <Carousel.Item>
-            <img width={1200} height={500} alt="Gdansk" src={dluga}/>
+            <img alt="Gdansk" src={dluga}/>
         </Carousel.Item>
         <Carousel.Item>
-            <img width={1200} height={500} alt="Gdansk" src={dluga}/>
+            <img alt="Gdansk" src={karuzela}/>
         </Carousel.Item>
     </Carousel>
 );
